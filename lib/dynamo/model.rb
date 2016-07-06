77dffc97b039359d2d8333a6e6890c0af40de61a
Douglas Rossignolli <douglas.rossignolli@gmail.com>
2016-07-05 21:49:15 -0300

    base

---
+++ lib/dynamo/model.rb
@@ -0,0 +1,7 @@
+require "dynamo/model/version"
+
+module Dynamo
+  module Model
+    # Your code goes here...
+  end
+end
---
+++ lib/dynamo/model/version.rb
@@ -0,0 +1,5 @@
+module Dynamo
+  module Model
+    VERSION = "0.1.0"
+  end
+end
