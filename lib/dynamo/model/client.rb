40be67ec337c254931e31107b51d90f48203bcf2
Douglas Rossignolli <douglas@kanamobi.com.br>
2016-07-13 15:07:04 -0300

    fix config

--- lib/dynamo/model/client.rb
+++ lib/dynamo/model/client.rb
@@ -4,10 +4,11 @@ module Dynamo
       attr_accessor :dynamodb, :config
 
       def initialize
-        @config = Configuration.configure do |config|
-          config.access_key_id = ENV["aws_access_key_id"]
-          config.secret_access_key = ENV["aws_secret_access_key"]
-          config.region = ENV["aws_region"]
+        @config = Configuration.configure do |current|
+          current.access_key_id = ENV["aws_access_key_id"]
+          current.secret_access_key = ENV["aws_secret_access_key"]
+          current.region = ENV["aws_region"]
+          current
         end
         setup
       end
@@ -16,7 +17,7 @@ module Dynamo
         @dynamodb = Aws::DynamoDB::Client.new(
           region: @config.region,
           access_key_id: @config.access_key_id,
-          secret_access_key: @configsecret_access_key
+          secret_access_key: @config.secret_access_key
         )
       end
     end
--- lib/dynamo/model/version.rb
+++ lib/dynamo/model/version.rb
@@ -1,5 +1,5 @@
 module Dynamo
   module Model
-    VERSION = "0.1.2p2"
+    VERSION = "0.1.2p3"
   end
 end
